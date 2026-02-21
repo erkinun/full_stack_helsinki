927ed6fd6b073a6825af8b0ec36171027053adca
Erkin Ünlü <erkinun@gmail.com>
2026-02-21 13:55:30 +0000

    part 1 exercise 2

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,15 +1,13 @@
 export function Content({part1, exercises1, part2, exercises2, part3, exercises3}) {
     return (
       <>
-          <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
+        <Part part={part1} exerciseCount={exercises1} />
+        <Part part={part2} exerciseCount={exercises2} />
+        <Part part={part3} exerciseCount={exercises3} />
       </>
     );
+}
+
+function Part({part, exerciseCount}) {
+  return (<p>{part} {exerciseCount}</p>)
 }
