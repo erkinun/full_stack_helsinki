8cff6cd74d268cf30e1df90c43f541c0c75671b2
Erkin Ünlü <erkinun@gmail.com>
2026-03-01 13:39:17 +0000

    part 2 exercise 1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <div>
+    {
+      props.parts.map((p) => (<Part part={p} />))
+    }
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p>total of {props.total} exercises</p>
+
+const Course = ({course: {name, parts}}) => {
+  return (
+    <div>
+      <Header course={name} />
+      <Content parts={parts} />
+      {/* <Total total={
+          parts.map(({exercises}) => exercises).reduce((prev, agg) => prev + agg, 0)} /> */}
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  }
+
+  return <Course course={course} />
+}
+
+export default App
