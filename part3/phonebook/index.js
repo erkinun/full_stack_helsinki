30df9fb53836e418b5698d4e448de33ad9c84438
Erkin Ünlü <erkinun@gmail.com>
2026-03-29 14:51:06 +0100

    part 3 exercise 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -38,6 +38,19 @@ app.get('/api/persons', (request, response) => {
   response.json(phonebookData)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = phonebookData.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
