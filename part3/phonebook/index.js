193d342871705e4e3a3ea9459bd4166d0d095b28
Erkin Ünlü <erkinun@gmail.com>
2026-03-29 15:08:00 +0100

    part 3 exercise 5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let phonebookData = [
   { 
@@ -57,6 +58,29 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  return Math.floor(Math.random() * 1_000_000);
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log({body: request.body})
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  phonebookData = phonebookData.concat(person)
+
+  response.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
