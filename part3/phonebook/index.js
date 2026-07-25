3f2c89b08a224a156736b0fe233c5bf2d935e9e4
Erkin Ünlü <erkinun@gmail.com>
2026-07-25 12:14:31 +0100

    formatting

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,101 +1,103 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
-const app = express()
-morgan.token('body', function (req, res) { return req.method === 'POST' ? JSON.stringify(req.body) : '' })
-app.use(morgan(':method :url :status :response-time ms :body'))
+const app = express();
+morgan.token("body", function (req, res) {
+  return req.method === "POST" ? JSON.stringify(req.body) : "";
+});
+app.use(morgan(":method :url :status :response-time ms :body"));
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static('dist'))
+app.use(express.json());
+app.use(cors());
+app.use(express.static("dist"));
 
 let phonebookData = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
   },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
-  { 
-    "id": "5",
-    "name": "Barry Poppendieck", 
-    "number": "39-23-6423122"
+  {
+    id: "5",
+    name: "Barry Poppendieck",
+    number: "39-23-6423122",
   },
 ];
 
-app.get('/info', (request, response) => {
-  response.send(`Phonebook has info for ${phonebookData.length} people <br/> ${new Date(Date.now())}`
-  )
-})
+app.get("/info", (request, response) => {
+  response.send(
+    `Phonebook has info for ${phonebookData.length} people <br/> ${new Date(Date.now())}`,
+  );
+});
 
-app.get('/api/persons', (request, response) => {
-  response.json(phonebookData)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(phonebookData);
+});
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = phonebookData.find(person => person.id === id)
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = phonebookData.find((person) => person.id === id);
 
   if (person) {
-    response.json(person)
+    response.json(person);
+  } else {
+    response.status(404).end();
   }
-  else {
-    response.status(404).end()
-  }
-})
+});
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  phonebookData = phonebookData.filter(person => person.id !== id)
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  phonebookData = phonebookData.filter((person) => person.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
 const generateId = () => {
   return Math.floor(Math.random() * 1_000_000);
-}
+};
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
   if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'name or number missing' 
-    })
+    return response.status(400).json({
+      error: "name or number missing",
+    });
   }
 
   if (phonebookData.find((p) => p.name === body.name)) {
     return response.status(400).json({
-      error: `${body.name} already in phonebook`
-    })
+      error: `${body.name} already in phonebook`,
+    });
   }
 
   const person = {
     name: body.name,
     number: body.number,
     id: generateId(),
-  }
+  };
 
-  phonebookData = phonebookData.concat(person)
+  phonebookData = phonebookData.concat(person);
 
-  response.json(person)
-})
+  response.json(person);
+});
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
